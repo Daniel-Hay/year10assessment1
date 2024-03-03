77185a4af8cf86b5eeef9a9ec63fe0855399c51b
Daniel-Hay <127155521+Daniel-Hay@users.noreply.github.com>
2024-03-04 09:53:38 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 import json 
 
-hello = "hi"
+api_key = '628b24e279558dc47b21fb160692cdcf'
+location = input("Enter a location: ")
 
-def main():
-    pass
+def weather_main(location):
+   response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}')
+   print(response.status_code)
+   print(response.json())
+   
+weather_main(location)
