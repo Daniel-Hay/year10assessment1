2eb94dcb5a7ae9197c834b640d3ed0f148a36119
Daniel-Hay <127155521+Daniel-Hay@users.noreply.github.com>
2024-03-04 10:06:09 +1100

    Forcast added

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 import json 
+import pprint
 
 api_key = '628b24e279558dc47b21fb160692cdcf'
 location = input("Enter a location: ")
 
 def weather_main(location):
-   response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}')
-   print(response.status_code)
-   print(response.json())
-   
+   response_current = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}')
+   response_forecast = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={location}&units=metric&appid={api_key}')
+   print(response_current.status_code)
+   print(response_current.json())
+   print(response_forecast.status_code)
+   pprint.pprint(response_forecast.json())
+
 weather_main(location)
