354026b40e9cdb507cb15455d0713b588a075a06
Daniel-Hay <127155521+Daniel-Hay@users.noreply.github.com>
2024-03-11 15:08:13 +1100

    Fixed name and added rich library

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
-import json 
+import json
+from rich import print
+from rich.console import Console
+from rich.theme import Theme
+
+# Custom Styles
+wrong_theme = Theme({"success": "green", "error": "red"})
+
 
 api_key = '628b24e279558dc47b21fb160692cdcf'
 location = input("Enter a location: ")
+history = []
+
+
+def intro():
+   print("Welcome to Daniel's Weather Console")
+
 
 # Main function with current weather, 5 day forecast and error handling
 def weather_main(location):
@@ -19,16 +32,19 @@ def weather_main(location):
 
       print(current_weather['main']['temp']) 
 
-      #print(response_current.status_code)
-      #print(response_current.json())
-      #print(response_forecast.status_code)
-      print(response_forecast.json())
-   
-   # Re-asks user to input a valid location
+      # print(response_current.status_code)
+      # print(response_current.json())
+      # print(response_forecast.status_code)
+      # print(response_forecast.json())
+
+      # prints current weather
+      print(f"The current weather in {location}")
+
+   # If invalid request, re-asks user to input a valid location
    else:
-      print("Status denied")
-      weather_main(location = input("Re-enter a location: "))
+      print("Status denied", style = "error")
+      location_new = input("Re-enter a location: ")
+      weather_main(location_new)
 
 
 weather_main(location)
-print(f"The current weather in {location}")
